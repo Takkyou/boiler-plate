66a03f307bdf9c02baf81793c07daec64c3a4312
Takkyou <takkyou@Takkyouui-MacBookPro.local>
2023-04-27 17:29:18 +0900

    remove .env

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require("./models/user");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
-mongoose
-  .connect(
-    "mongodb+srv://takelucky777:dksk21121234$a@cluster0.cg3wch9.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("MongoDB");
-  });
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.post("/register", async (req, res) => {
+  const user = await User.create(req.body);
+  res.send(user);
 });
 
 app.listen(port, () => {
